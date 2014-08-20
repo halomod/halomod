755eb659300d71fd949ecbb1294ff576d8791ad6
Steven Murray <steven.jeanette.m@gmail.com>
2014-08-21 00:16:56 +0800

    simple message for debug

--- halomod/fit.py
+++ halomod/fit.py
@@ -257,6 +257,7 @@ def fit_hod(data, priors, h, guess=[], nwalkers=100, nsamples=100, burnin=0,
     i = 0
     for prior in priors:
         if isinstance(prior, Uniform):
+            print prior.name, guess[i], prior.low, prior.high
             stacked_val[:, i] += np.random.normal(loc=0.0, scale=0.05 *
                                                   min((guess[i] - prior.low),
                                                       (prior.high - guess[i])),
