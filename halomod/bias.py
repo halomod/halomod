95c77208428637f2454ed30ddf67ca1bef1bf4a5
Steven Murray <steven.jeanette.m@gmail.com>
2015-03-06 16:12:36 +0800

    made bias models compliable with Models

--- halomod/bias.py
+++ halomod/bias.py
@@ -5,75 +5,93 @@ Created on Oct 1, 2013
 '''
 import numpy as np
 import sys
-from hmf.filters import get_filter
+from hmf._framework import Model
 _allmodels = ["ST", "seljak", 'ma', 'tinker05', 'tinker10']
 
-def get_bias(model):
-    """
-    A function that chooses the correct bias function and returns it
-    """
-    try:
-        return getattr(sys.modules[__name__], model)
-    except AttributeError:
-        raise
-        raise AttributeError(str(model) + "  is not a valid bias model")
+class Bias(Model):
+    def __init__(self, nu, delta_c, delta_halo, n, **model_parameters):
+        self.nu = nu
+        self.n = n
+        self.delta_c = delta_c
+        self.delta_halo = delta_halo
+        super(Bias, self).__init__(**model_parameters)
 
-def ST(hmod, **params):
+    def bias(self):
+        pass
+
+class ST(Bias):
     """
     Sheth-Tormen bias
     """
+    _defaults = {"q":0.707, "p":0.3}
 
-    q = params.get("q", 0.707)
-    p = params.get("p", 0.3)
-    f = get_filter("TopHat", rho_mean=hmod.mean_dens,
-                        delta_c=hmod.delta_c, lnk=hmod.lnk, lnp=hmod.power)
-    nu = f.nu(f.mass_to_radius(hmod.M))
-#     nu = hmod.nu
-    return 1 + (q * nu - 1) / hmod.delta_c + (2 * p / hmod.delta_c) / (1 + (q * nu) ** p)
+    def bias(self):
+        p = self.params['p']
+        q = self.params['q']
+        return 1 + (q * self.nu - 1) / self.delta_c + (2 * p / self.delta_c) / (1 + (q * self.nu) ** p)
 
-def seljak(hmod):
+class Seljak(ST):
     """
-    Large scale bias b(M)
+    Seljak bias
     """
-    return 1 + (hmod.nu - 1) / hmod.delta_c + 0.6 / (hmod.delta_c * (1 + (0.707 * hmod.nu) ** 0.3))
+    def bias(self):
+        p = self.params['p']
+        q = self.params['q']
+        return 1 + (self.nu - 1) / self.delta_c + (2 * p / self.delta_c) / (1 + (q * self.nu) ** p)
 
-def ma(hmod):
-    return (1 + (hmod.nu - 1) / hmod.delta_c) * (1 / (2 * hmod.nu ** 2) + 1) ** (0.06 - 0.02 * hmod.n)
+class Ma(Bias):
+    """
+    Ma bias
+    """
+    _defaults = {"a":0.06, "b":0.02}
 
-# def _bias_sw(hmod):
-#     raise ValueError("Seljak-Warren Not Implemented Yet")
-#        siginv = spline(hmod.sigma, hmod.M)
-#        mstar = siginv(dc)
-#        x = hmod.M / mstar
-#        bias_ls = 0.53 + 0.39 * x ** 0.45 + 0.13 / (40 * x + 1) + 5E-4 * x ** 1.5
-#
-#        #TODO: what the monkeys is alpha_s??
-#        return bias_ls + np.log10(x) * (0.4 * (hmod.cosmo_params['omegam'] - 0.3 + hmod.cosmo_params['n'] - 1) + 0.3 * (hmod.cosmo_params['sigma_8'] - 0.9 + hmod.cosmo_params['H0'] / 100 - 0.7) + 0.8)
+    def bias(self):
+        a = self.params['a']
+        b = self.params['b']
+        return (1 + (self.nu - 1) / self.delta_c) * (1 / (2 * self.nu ** 2) + 1) ** (a - b * self.n)
 
-def tinker05(hmod):
-    a = 0.707
-    sa = np.sqrt(a)
-    b = 0.35
-    c = 0.8
-    return 1 + 1 / (sa * hmod.delta_c) * (sa * (a * hmod.nu) + sa * b * (a * hmod.nu) ** (1 - c) - (a * hmod.nu) ** c / ((a * hmod.nu) ** c + b * (1 - c) * (1 - c / 2)))
+class Tinker05(Bias):
+    _defaults = {"a":0.707, "b":0.35, "c":0.8}
 
-def tinker10(hmod):
-    f = get_filter("TopHat", rho_mean=hmod.mean_dens,
-                    delta_c=hmod.delta_c, lnk=hmod.lnk, lnp=hmod.power)
-    nu = f.nu(f.mass_to_radius(hmod.M))
-    y = np.log10(hmod.delta_halo)
-    A = 1.0 + 0.24 * y * np.exp(-(4 / y) ** 4)
-    a = 0.44 * y - 0.88
-    B = 0.183
-    b = 1.5
-    C = 0.019 + 0.107 * y + 0.19 * np.exp(-(4 / y) ** 4)
-    c = 2.4
+    def bias(self):
+        a = self.params['a']
+        sa = np.sqrt(a)
+        b = self.params['b']
+        c = self.params['c']
+        return 1 + 1 / (sa * self.delta_c) * (sa * (a * self.nu) + sa * b * (a * self.nu) ** (1 - c) - (a * self.nu) ** c / ((a * self.nu) ** c + b * (1 - c) * (1 - c / 2)))
 
-#         print y, A, a, B, b, C, c
-    return 1 - A * nu ** a / (nu ** a + hmod.delta_c ** a) + B * nu ** b + C * nu ** c
+class Tinker10(Bias):
+    _defaults = {"B":0.183, "b":1.5, "c":2.4}
 
-def bias_scale(hmod, xi_dm):
+    def bias(self):
+        y = np.log10(self.delta_halo)
+        A = 1.0 + 0.24 * y * np.exp(-(4 / y) ** 4)
+        a = 0.44 * y - 0.88
+        C = 0.019 + 0.107 * y + 0.19 * np.exp(-(4 / y) ** 4)
+
+        B = self.params['B']
+        c = self.params['c']
+        b = self.params['b']
+        return 1 - A * self.nu ** a / (self.nu ** a + self.delta_c ** a) + B * self.nu ** b + C * self.nu ** c
+
+
+class ScaleDepBias(Model):
+    def __init__(self, xi_dm, **model_parameters):
+        self.xi_dm = xi_dm
+        super(ScaleDepBias, self).__init__(**model_parameters)
+
+    def bias_scale(self):
+        pass
+
+class Tinker_SD05(ScaleDepBias):
     """
     Scale-dependent bias, Tinker 2005
     """
-    return np.sqrt(hmod.bias ** 2 * (1 + 1.17 * xi_dm) ** 1.49 / (1 + 0.69 * xi_dm) ** 2.09)
+    _defaults = {"a":1.17, "b":1.49, "c":0.69, "d":2.09}
+
+    def bias_scale(self):
+        a = self.params['a']
+        b = self.params['b']
+        c = self.params['c']
+        d = self.params['d']
+        return np.sqrt((1 + a * self.xi_dm) ** b / (1 + c * self.xi_dm) ** d)
