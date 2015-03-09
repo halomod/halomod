404e1da01b60f7cfd28961146e2e833816238d8f
Steven Murray <steven.jeanette.m@gmail.com>
2015-03-09 15:02:28 +0800

    concentration models updated

--- halomod/concentration.py
+++ halomod/concentration.py
@@ -3,31 +3,11 @@ Created on 08/12/2014
 
 @author: Steven
 '''
-import copy
 from scipy.interpolate import InterpolatedUnivariateSpline as spline
-from hmf.tools import growth_factor
 import numpy as np
-import sys
+from hmf._framework import Model
 
-def get_cm(name, **kwargs):
-    """
-    Returns the correct subclass of :class:`CMRelation`.
-    
-    Parameters
-    ----------
-    name : str
-        The class name of the appropriate fit
-        
-    \*\*kwargs : 
-        Any parameters for the instantiated fit (including model parameters)
-    """
-    try:
-        return getattr(sys.modules[__name__], name)(**kwargs)
-    except AttributeError:
-        raise AttributeError(str(name) + "  is not a valid CMRelation class")
-
-
-class CMRelation(object):
+class CMRelation(Model):
     r"""
     Base-class for Concentration-Mass relations
     
@@ -68,36 +48,24 @@ class CMRelation(object):
     _defaults = {}
 
     use_cosmo = False
-    def __init__(self, filter, delta_c, z, cdict, **model_parameters):
+    def __init__(self, nu, z, growth, M, **model_parameters):
         """
         filter : a filter function at z = 0
         """
-        # Check that all parameters passed are valid
-        for k in model_parameters:
-            if k not in self._defaults:
-                raise ValueError("%s is not a valid argument for the %s CMRelation" % (k, self.__class__.__name__))
-
-        # Gather model parameters
-        self.params = copy.copy(self._defaults)
-        self.params.update(model_parameters)
-
         # Save instance variables
-        self.filter = filter
-        self.delta_c = delta_c
+        self.nu = nu
         self.z = z
-        self.cdict = cdict
+        self.growth = growth
+        self.M = M
+
+        super(CMRelation, self).__init__(**model_parameters)
 
 class Bullock01(CMRelation):
     _defaults = {"F":0.001, "K":3.4}
 
     def zc(self, m):
-
-        ms = self.params["F"] * m
-        rs = self.filter.mass_to_radius(ms)
-        sigma = self.filter.sigma(rs)
-        g = growth_factor(100.0, self.cdict, True)
-        s = spline(g[1, :], g[0, :])
-        zc = s(self.delta_c / sigma)
+        g = self.growth.growth_factor_fn(inverse=True)
+        zc = g(np.sqrt(self.nu))
         zc[zc < 0] = 0.0  # hack?
         return zc
 
@@ -107,25 +75,19 @@ class Bullock01(CMRelation):
 class Cooray(CMRelation):
     _defaults = {"a":9.0, "b":0.13, "c":1.0, "ms":None}
     def ms(self):
-        ms = np.logspace(5, 15, 1000)
-        rs = self.filter.mass_to_radius(ms)
-        sigma = self.filter.sigma(rs)
-        d = sigma[1:] - sigma[:-1]
+        d = self.nu[1:] - self.nu[:-1]
         try:
             # this to start below "saturation level" in sharp-k filters.
-            pos = np.where(d > 0)[0][-1]
+            pos = np.where(d < 0)[0][-1]
         except IndexError:
             pos = 0
-        sigma = sigma[pos:]
-        ms = ms[pos:]
-        s = spline(sigma[::-1], ms[::-1])
-        return s(self.delta_c)
+        nu = self.nu[pos:]
+        ms = self.M[pos:]
+        s = spline(nu, ms)
+        return s(1.0)
 
     def cm(self, m):
-        if self.params['ms'] is None:
-            ms = self.ms()
-        else:
-            ms = self.params['ms']
+        ms = self.params['ms'] or self.ms()
         return self.params['a'] / (1 + self.z) ** self.params['c'] * (ms / m) ** self.params['b']
 
 class Duffy(Cooray):
