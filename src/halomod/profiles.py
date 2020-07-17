61c9f9dbdcca53daf3389d36abe2d6206c21ad01
Zhaoting <ztchenustc@gmail.com>
2020-07-17 17:21:49 +0100

    test

--- src/halomod/profiles.py
+++ src/halomod/profiles.py
@@ -34,7 +34,7 @@ You can also specify a different profile for tracer if you're working with
     >>> from halomod.halo_model import TracerHaloModel
     >>> hm = TracerHaloModel(halo_profile_model="NFW",tracer_profile_model="CoredNFW")
 
-Notice that tracer density profile density should be used only in inverse volume or dimensionless  unit.
+Notice that tracer density profile density should be used only in inverse volume or dimensionless unit.
 """
 import numpy as np
 import scipy.special as sp
